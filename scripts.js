fab0d53c801ddfd8541e514f464a311c228b5fa5
Ripon-Patgiri <riponmongzeell@gmail.com>
2023-03-24 19:40:46 +0530

    added react and wrote render function

---
+++ scripts.js
@@ -0,0 +1,2 @@
+ReactDOM.render(<h1>Hello Everyone!</h1>, document.getElementById("root")); // Allows me render something that looks very much like HTML
+// Parameters : what to render, where to render
